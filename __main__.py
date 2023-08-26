80889d5cc75effd51a9b46d6be7f9b167f138157
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2023-08-26 15:21:08 +0200

    updated first draft of Dash app

--- __main__.py
+++ __main__.py
@@ -1,32 +1,44 @@
 from dash import Dash, dcc, html, dash_table, callback, Output, Input
-from financial_ratios import Stock
+import financial_ratios as fr
 import dash_bootstrap_components as dbc
 import os
 
 key = os.getenv("API_Polygon")
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-Stock = Stock(key, "AAPL")
-
 app.layout = html.Div([
     html.Div(children="Portfolio Stack"),
     html.Hr(),
     dcc.RadioItems(options=['AAPL', 'TSLA', 'AMZN'], value='AAPL', id='ticker_as_text'),
-    dash_table.DataTable(data=[{"E/P Ratio": 0,
-                               "P/B Ratio": 0,
-                               "Current Ratio": 0,
-                               "ROE": 0,
-                               "ROA": 0,
-                               "Average Dividend growth": 0,
-                               }],
-                         columns=[{"name": i, "id": i} for i in [
-                             "E/P Ratio",
-                             "P/B Ratio",
-                             "Current Ratio",
-                             "ROE",
-                             "ROA",
-                             "Average Dividend growth"]], page_size=6)
+    dash_table.DataTable(data=[],
+                         columns=[],
+                         page_size=6,
+                         id="ratio_table"),
 ])
+
+
+@callback(Output("ratio_table", "data"),
+          Input("ticker_as_text", "value"))
+def update_table(ticker):
+
+    stock = fr.Stock(key, "AAPL", "Stock")
+
+    data = [{"E/P Ratio": stock.ep_ratio(),
+             "P/B Ratio": stock.pb_ratio(),
+             "Current Ratio": stock.current_ratio(),
+             "ROE": stock.ro_equity(),
+             "ROA": stock.ro_assets(),
+             "Average Dividend growth": stock.div_growth()}]
+
+    columns = [{"name": i, "id": i} for i in [
+        "E/P Ratio",
+        "P/B Ratio",
+        "Current Ratio",
+        "ROE",
+        "ROA",
+        "Average Dividend growth"]]
+
+    return data, columns
 
 
 if __name__ == "__main__":
