73f1940d17fc2ea607238712296f43339fd084ca
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2023-08-28 06:46:08 +0200

    Added fetch_value, search field and search button

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
-from dash import Dash, dcc, html, dash_table, callback, Output, Input
+from dash import Dash, dcc, html, dash_table, callback, Output, Input, State
 import financial_ratios as fr
 import dash_bootstrap_components as dbc
+import pandas as pd
 import os
 
 key = os.getenv("API_Polygon")
@@ -9,11 +10,9 @@ app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div([
     html.Div(children="Portfolio Stack"),
     html.Hr(),
-    dcc.RadioItems(options=[
-        {'label': 'AAPL', 'value': 'AAPL'},
-        {'label': 'TSLA', 'value': 'TSLA'},
-        {'label': 'AMZN', 'value': 'AMZN'}
-    ], value="AAPL", id='ticker_as_text'),
+    dcc.Input(id="ticker_as_text".format("search"),
+              value="AAPL".format("search")),
+    html.Button('Submit', id='search_button'),
     dash_table.DataTable(data=[],
                          columns=[{"name": i, "id": i} for i in [
                             "E/P Ratio",
@@ -28,19 +27,25 @@ app.layout = html.Div([
 
 
 @callback(Output("ratio_table", "data"),
-          Input("ticker_as_text", "value"))
-def update_table(ticker):
+          Input("search_button", "n_clicks"),
+          State("ticker_as_text", "value"))
+def update_table(n_clicks, ticker):
 
     stock = fr.Stock(key, ticker, "Stock")
 
-    data = [{"E/P Ratio": stock.ep_ratio(),
-             "P/B Ratio": stock.pb_ratio(),
-             "Current Ratio": stock.current_ratio(),
-             "ROE": stock.ro_equity(),
-             "ROA": stock.ro_assets(),
-             "Average Dividend growth": stock.div_growth()}]
+    def fetch_value(object_method, default_value):
+        try:
+            return object_method()
+        except Exception as e:
+            print(e)
+            return default_value
 
-    print(data)
+    data = [{"E/P Ratio": fetch_value(stock.ep_ratio, pd.NA),
+             "P/B Ratio": fetch_value(stock.pb_ratio, pd.NA),
+             "Current Ratio": fetch_value(stock.current_ratio, pd.NA),
+             "ROE": fetch_value(stock.ro_equity, pd.NA),
+             "ROA": fetch_value(stock.ro_assets, pd.NA),
+             "Average Dividend growth": fetch_value(stock.div_growth, pd.NA)}]
 
     return data
 
