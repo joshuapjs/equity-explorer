d25323559f126928022e81d12bd1b55659972b3f
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2023-09-05 00:40:15 +0200

    Added first version of get_sharpe_ratio

--- quant_ratios.py
+++ quant_ratios.py
@@ -25,7 +25,6 @@ def get_capm(api_key, asset_ticker, freq=1):
     spy_returns.index = spy_returns.index.normalize()
 
     concatenated_returns = pd.concat([asset_returns, spy_returns], axis=1).dropna()
-    concatenated_returns.to_excel("returns.xlsx")
 
     market_portfolio = sm.add_constant(concatenated_returns["SPX"])
     stock_returns = concatenated_returns[asset_ticker]
@@ -40,3 +39,19 @@ def get_realized_volatility(api_key, asset_ticker, freq=1):
     asset_prices = asset.get_prices()["c"]
     asset_returns = asset_prices.pct_change(periods=freq).dropna()
     return asset_returns.std() * np.sqrt(252)
+
+
+def get_sharpe_ratio(api_key, assert_ticker):
+    asset = Asset(api_key, assert_ticker, "Stock",
+                  start=(datetime.today() - timedelta(days=252)).strftime('%Y-%m-%d'))
+    asset_prices = asset.get_prices()["c"].to_list()
+    asset_return = (asset_prices[-1] - asset_prices[0]) / asset_prices[0]
+    print(asset_return)
+    asset_volatility = get_realized_volatility(api_key, assert_ticker)
+    print(asset_volatility)
+    risk_free_rate = 0.0538  # 53 Weeks T-Bill rate as of 2023-09-05
+    sharpe_ratio = (asset_return - risk_free_rate) / asset_volatility
+
+    return sharpe_ratio
+
+
