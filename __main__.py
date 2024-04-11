f26319304ea070840868616fb75a4a4db27a9556
Joshua Spingler <82243579+joshuapjs@users.noreply.github.com>
2024-04-11 19:58:54 +0200

    small adjustment

--- __main__.py
+++ __main__.py
@@ -1,6 +1,5 @@
 from dash import Dash, dcc, html, dash_table, callback, Output, Input, State
-# from price_data import Asset
-from . import price_data.Asset
+from price_data import Asset
 import quant_ratios as qr
 import fundamental_ratios as fr
 import dash_bootstrap_components as dbc
